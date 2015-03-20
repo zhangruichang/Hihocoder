53a9ec535943d279553019c81a1376f0d8e4b57e
zhangruichang <zhangruichang112@gmail.com>
2015-03-20 17:25:35 +0800

    ZeroOne Bag optimization code

---
+++ HihoCodePro1038/main.cpp
@@ -0,0 +1,69 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include<set>
+#include<map>
+#include<list>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<climits>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<iostream>
+#include<algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+#define f first
+#define s second
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    if(!m) return n;
+    return GCD(n%m, m);//yushu and chushu
+}
+int w[maxn],v[maxn],dp[maxn];
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    int n, wei;
+    while(cin>>n>>wei)
+    {
+        for(int i=0;i<n;i++) cin>>w[i]>>v[i];
+        for(int i=1;i<=n;i++) for(int j=wei;j>=w[i-1];j--)
+                dp[j]=max(dp[j], dp[j-w[i-1]]+v[i-1]);
+        cout<<dp[wei]<<endl;
+    }
+	return 0;
+}
